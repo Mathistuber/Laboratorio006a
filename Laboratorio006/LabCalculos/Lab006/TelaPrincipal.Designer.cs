5b8c4b8085de22450e307230f2552fd25aac8d45
matheus.dlagares <matheus.dlagares@SBC0601504W10-1>
2022-05-03 16:15:34 -0300

    AtualizaçãoAdicionar arquivos de projeto.

---
+++ Laboratorio006/LabCalculos/Lab006/TelaPrincipal.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace Laboratorio006.LabCalculos.Lab006
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblValor2 = new System.Windows.Forms.Label();
+            this.lblValor3 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.nudValor1 = new System.Windows.Forms.NumericUpDown();
+            this.nudValor3 = new System.Windows.Forms.NumericUpDown();
+            this.nudValor2 = new System.Windows.Forms.NumericUpDown();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Location = new System.Drawing.Point(79, 83);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(42, 15);
+            this.lblValor1.TabIndex = 0;
+            this.lblValor1.Text = "Valor 1";
+            // 
+            // lblValor2
+            // 
+            this.lblValor2.AutoSize = true;
+            this.lblValor2.Location = new System.Drawing.Point(79, 133);
+            this.lblValor2.Name = "lblValor2";
+            this.lblValor2.Size = new System.Drawing.Size(42, 15);
+            this.lblValor2.TabIndex = 1;
+            this.lblValor2.Text = "Valor 2";
+            // 
+            // lblValor3
+            // 
+            this.lblValor3.AutoSize = true;
+            this.lblValor3.Location = new System.Drawing.Point(79, 183);
+            this.lblValor3.Name = "lblValor3";
+            this.lblValor3.Size = new System.Drawing.Size(42, 15);
+            this.lblValor3.TabIndex = 2;
+            this.lblValor3.Text = "Valor 3";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResultado.Font = new System.Drawing.Font("Sylfaen", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResultado.ImageKey = "(nenhum)";
+            this.lblResultado.Location = new System.Drawing.Point(79, 272);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(191, 99);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // nudValor1
+            // 
+            this.nudValor1.Location = new System.Drawing.Point(150, 81);
+            this.nudValor1.Name = "nudValor1";
+            this.nudValor1.Size = new System.Drawing.Size(120, 23);
+            this.nudValor1.TabIndex = 4;
+            // 
+            // nudValor3
+            // 
+            this.nudValor3.Location = new System.Drawing.Point(150, 181);
+            this.nudValor3.Name = "nudValor3";
+            this.nudValor3.Size = new System.Drawing.Size(120, 23);
+            this.nudValor3.TabIndex = 5;
+            // 
+            // nudValor2
+            // 
+            this.nudValor2.Location = new System.Drawing.Point(150, 131);
+            this.nudValor2.Name = "nudValor2";
+            this.nudValor2.Size = new System.Drawing.Size(120, 23);
+            this.nudValor2.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(136, 230);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(359, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.nudValor2);
+            this.Controls.Add(this.nudValor3);
+            this.Controls.Add(this.nudValor1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblValor3);
+            this.Controls.Add(this.lblValor2);
+            this.Controls.Add(this.lblValor1);
+            this.Name = "TelaPrincipal";
+            this.ShowIcon = false;
+            this.Text = "Sistema de Calculo";
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValor2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblValor1;
+        private System.Windows.Forms.Label lblValor2;
+        private System.Windows.Forms.Label lblValor3;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.NumericUpDown nudValor1;
+        private System.Windows.Forms.NumericUpDown nudValor3;
+        private System.Windows.Forms.NumericUpDown nudValor2;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ Laboratorio006/LabCalculos/Lab007/TelaPrincipal.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Laboratorio006.LabCalculos.Lab007
+{
+    partial class TelaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaPrincipal));
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.lblFrete = new System.Windows.Forms.Label();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.tbxCliente = new System.Windows.Forms.TextBox();
+            this.cbxUF = new System.Windows.Forms.ComboBox();
+            this.tbxFrete = new System.Windows.Forms.TextBox();
+            this.tbxValor = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(81, 92);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(44, 15);
+            this.lblCliente.TabIndex = 0;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(81, 147);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(33, 15);
+            this.lblValor.TabIndex = 1;
+            this.lblValor.Text = "Valor";
+            // 
+            // lblFrete
+            // 
+            this.lblFrete.AutoSize = true;
+            this.lblFrete.Location = new System.Drawing.Point(81, 201);
+            this.lblFrete.Name = "lblFrete";
+            this.lblFrete.Size = new System.Drawing.Size(46, 15);
+            this.lblFrete.TabIndex = 2;
+            this.lblFrete.Text = "% Frete";
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Location = new System.Drawing.Point(278, 147);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(24, 15);
+            this.lblUF.TabIndex = 3;
+            this.lblUF.Text = "UF:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblResultado.Location = new System.Drawing.Point(81, 293);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(358, 102);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // tbxCliente
+            // 
+            this.tbxCliente.Location = new System.Drawing.Point(131, 89);
+            this.tbxCliente.Name = "tbxCliente";
+            this.tbxCliente.Size = new System.Drawing.Size(310, 23);
+            this.tbxCliente.TabIndex = 5;
+            // 
+            // cbxUF
+            // 
+            this.cbxUF.FormattingEnabled = true;
+            this.cbxUF.Items.AddRange(new object[] {
+            "AC",
+            "AL",
+            "AP",
+            "AM",
+            "BA",
+            "CE",
+            "ES",
+            "GO",
+            "MA",
+            "MT",
+            "MS",
+            "MG",
+            "PA",
+            "PB",
+            "PR",
+            "PE",
+            "PI",
+            "RJ",
+            "RN",
+            "RS",
+            "RO",
+            "RR",
+            "SC",
+            "SP",
+            "SE",
+            "TO",
+            "DF"});
+            this.cbxUF.Location = new System.Drawing.Point(308, 144);
+            this.cbxUF.Name = "cbxUF";
+            this.cbxUF.Size = new System.Drawing.Size(131, 23);
+            this.cbxUF.TabIndex = 6;
+            // 
+            // tbxFrete
+            // 
+            this.tbxFrete.Location = new System.Drawing.Point(129, 198);
+            this.tbxFrete.Name = "tbxFrete";
+            this.tbxFrete.Size = new System.Drawing.Size(310, 23);
+            this.tbxFrete.TabIndex = 7;
+            // 
+            // tbxValor
+            // 
+            this.tbxValor.Location = new System.Drawing.Point(120, 144);
+            this.tbxValor.Name = "tbxValor";
+            this.tbxValor.Size = new System.Drawing.Size(129, 23);
+            this.tbxValor.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(81, 242);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(358, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // TelaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(524, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.tbxValor);
+            this.Controls.Add(this.tbxFrete);
+            this.Controls.Add(this.cbxUF);
+            this.Controls.Add(this.tbxCliente);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblUF);
+            this.Controls.Add(this.lblFrete);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblCliente);
+            this.Name = "TelaPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TelaPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label lblFrete;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox tbxCliente;
+        private System.Windows.Forms.ComboBox cbxUF;
+        private System.Windows.Forms.TextBox tbxFrete;
+        private System.Windows.Forms.TextBox tbxValor;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
